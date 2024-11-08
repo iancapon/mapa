862eb43d3e66a3ac78f8122dded5612506381729
iancapon <89599951+iancapon@users.noreply.github.com>
2024-11-08 15:03:50 -0300

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -6,6 +6,10 @@ let mapa = undefined
 let colectivos = undefined
 let zoom = 1
 
+let p_x = -58.463843; // Longitud o posición X
+let p_y = -34.635661; // Latitud o posición Y
+let radio = 200; // Radio del círculo
+
 function preload() {
     assets.caba = loadJSON("./map_assets/callejero.JSON")
     assets.lineas = loadJSON("./map_assets/lineas.JSON")
@@ -39,4 +43,33 @@ function draw() {
     strokeWeight(1)
     fill(0)
     text("RED DE COLECTIVO", 1500 * 2, 200 * 2)
+        dibujarCirculo(p_x, p_y, radio); // Ejemplo de coordenadas y radio
+}
+
+function calcularDistancia(coord1, coord2) {
+        const R = 6371000; // Radio de la Tierra en metros
+        const lat1 = coord1[1] * (Math.PI / 180);
+        const lon1 = coord1[0] * (Math.PI / 180);
+        const lat2 = coord2[1] * (Math.PI / 180);
+        const lon2 = coord2[0] * (Math.PI / 180);
+        const dLat = lat2 - lat1;
+        const dLon = lon2 - lon1;
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(lat1) * Math.cos(lat2) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+     
+ return R * c; // distancia en metros
+    }
+
+function dibujarCirculo(longitud, latitud, radio) {
+    // Convierte las coordenadas del mapa a la escala actual
+    const escala = mapa.getEscala();
+    const x = (longitud + escala.desvioX) * escala.escala;
+    const y = (escala.factor / escala.escala - (latitud + escala.desvioY)) * escala.escala;
+    // Dibujar el círculo en el punto escalado
+    noFill();
+    stroke(255, 0, 0); // Color del borde
+    strokeWeight(2); // Grosor del borde
+    ellipse(x, y, radio * 2); // Dibuja el círculo
 }
