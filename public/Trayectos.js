6f6b23bb3377ad56fea8c41d8fe24e1bfd02319c
iancapon <iancaponrusso@gmail.com>
2024-11-08 21:35:04 -0300

    muchas cosiyas, elegir el color de las lineas

--- public/Trayectos.js
+++ public/Trayectos.js
@@ -1,10 +1,16 @@
 const Trayectos = function (assets, escala) {
     this.lineas_cubiertas = []
+    this.colores = []
     this.dibujar_mapa = function () {
         this.renderizar(this.lineas_cubiertas)
     }
 
-    this.cargar_linea = function (linea, recorrido) {
+    this.cargar_linea = function (linea, recorrido, color) {
+        if (color != undefined) {
+            this.colores.push({ linea: linea, recorrido: recorrido, color: color })
+        } else {
+            this.colores.push({ linea: linea, recorrido: recorrido, color: random(0, 100) })
+        }
         let linea_gral = this.buscar_lineas(assets.lineas, linea)
         if (recorrido == undefined) {
             linea_gral.forEach(line => this.lineas_cubiertas.push(line))
@@ -22,8 +28,8 @@ const Trayectos = function (assets, escala) {
         try {
             datos.forEach((feature, a_index) => {
                 colorMode(HSB, 100)
-                stroke(random(0, 100), 100, 100)
-                strokeWeight(8)
+                stroke(this.colores.find(col => col.linea === feature.properties.linea).color, 90, 90,70)
+                strokeWeight(13)
                 feature.geometry.coordinates.forEach(recorrido => {
                     let prev_point
                     recorrido.forEach((point, p_index) => {
@@ -72,7 +78,7 @@ const Trayectos = function (assets, escala) {
         this.lineas_cubiertas = lineasCercanas;
     }
 
-    
+
 
     this.buscar_instancias_de_tipo_recorrido = function (arreglo, numero) {
         return arreglo.filter(feature => feature.properties.linea == numero)
@@ -94,4 +100,33 @@ const Trayectos = function (assets, escala) {
             }
         })
     }
+}
+
+function calcularDistancia(coord1, coord2) {
+    const R = 6371000; // Radio de la Tierra en metros
+    const lat1 = coord1[1] * (Math.PI / 180);
+    const lon1 = coord1[0] * (Math.PI / 180);
+    const lat2 = coord2[1] * (Math.PI / 180);
+    const lon2 = coord2[0] * (Math.PI / 180);
+    const dLat = lat2 - lat1;
+    const dLon = lon2 - lon1;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1) * Math.cos(lat2) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // distancia en metros
+}
+
+
+function dibujarCirculo(longitud, latitud, radio) {
+    // Convierte las coordenadas del mapa a la escala actual
+    const escala = mapa.getEscala();
+    const x = (longitud + escala.desvioX) * escala.escala;
+    const y = (escala.factor / escala.escala - (latitud + escala.desvioY)) * escala.escala;
+    // Dibujar el círculo en el punto escalado
+    noFill();
+    stroke(255, 0, 0); // Color del borde
+    strokeWeight(2); // Grosor del borde
+    ellipse(x, y, radio * 2); // Dibuja el círculo
 }
--- public/sketch.js
+++ public/sketch.js
@@ -6,9 +6,9 @@ let mapa = undefined
 let colectivos = undefined
 let zoom = 1
 
-let p_x = -58.463843; // Longitud o posición X
-let p_y = -34.635661; // Latitud o posición Y
-let radio = 200; // Radio del círculo
+let p_x = -58.404665117856574; ////longitud
+let p_y = -34.58673430613933;  /// latitud
+let radio = 368; // Radio del círculo
 
 function preload() {
     assets.caba = loadJSON("./map_assets/callejero.JSON")
@@ -19,11 +19,13 @@ function setup() {
     mapa = new Mapa(assets, 800 * 4)
     colectivos = new Trayectos(assets, mapa.getEscala())
 
-    colectivos.cargar_linea("111","A")
-    colectivos.cargar_linea("152","A")
-    colectivos.cargar_linea("29","A")
-    colectivos.cargar_linea("61","A")
-    colectivos.cargar_linea("62","A")
+    //colectivos.cargar_lineas_cercanas([p_x,p_y],radio)
+    colectivos.cargar_linea("111","A",20)
+    colectivos.cargar_linea("152","A",73)
+    colectivos.cargar_linea("062","A",39)
+    colectivos.cargar_linea("039","A",8)
+    colectivos.cargar_linea("029","A",16)
+    colectivos.cargar_linea("092","A",48)
 
     createCanvas(1920 * 2, 1080 * 2)
     noLoop()
@@ -42,34 +44,19 @@ function draw() {
     stroke(0)
     strokeWeight(1)
     fill(0)
-    text("RED DE COLECTIVO", 1500 * 2, 200 * 2)
-        dibujarCirculo(p_x, p_y, radio); // Ejemplo de coordenadas y radio
+    text("RED DE TRANSPORTE", 1500 * 2, 200 * 2)
+
+    //dibujarMarcador(p_x, p_y); 
 }
 
-function calcularDistancia(coord1, coord2) {
-        const R = 6371000; // Radio de la Tierra en metros
-        const lat1 = coord1[1] * (Math.PI / 180);
-        const lon1 = coord1[0] * (Math.PI / 180);
-        const lat2 = coord2[1] * (Math.PI / 180);
-        const lon2 = coord2[0] * (Math.PI / 180);
-        const dLat = lat2 - lat1;
-        const dLon = lon2 - lon1;
-        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.cos(lat1) * Math.cos(lat2) *
-            Math.sin(dLon / 2) * Math.sin(dLon / 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-     
- return R * c; // distancia en metros
-    }
 
-function dibujarCirculo(longitud, latitud, radio) {
+function dibujarMarcador(longitud, latitud) {
     // Convierte las coordenadas del mapa a la escala actual
     const escala = mapa.getEscala();
     const x = (longitud + escala.desvioX) * escala.escala;
     const y = (escala.factor / escala.escala - (latitud + escala.desvioY)) * escala.escala;
     // Dibujar el círculo en el punto escalado
-    noFill();
-    stroke(255, 0, 0); // Color del borde
+    fill(100,100,100);
     strokeWeight(2); // Grosor del borde
-    ellipse(x, y, radio * 2); // Dibuja el círculo
+    ellipse(x, y, 40); // Dibuja el círculo
 }
